1bd6be1b64a0a7ec4d6a7a1cc548d0c899f19a11
Kd <86334894+kdlanguido@users.noreply.github.com>
2026-01-10 04:10:13 +0800

    hahaha!

--- app/(authenticated)/training/toproll/page.tsx
+++ app/(authenticated)/training/toproll/page.tsx
@@ -68,30 +68,30 @@ export default async function Page() {
 
                         <AccordionItem value="item-1">
                             <AccordionTrigger>What is Toproll?</AccordionTrigger>
-                            <AccordionContent className="flex flex-col gap-4 text-balance">
+                            <AccordionContent className="flex flex-col gap-4">
                                 <WhatIsToproll />
                             </AccordionContent>
                         </AccordionItem>
 
                         <AccordionItem value="item-2">
                             <AccordionTrigger>Key Techniques in Toproll?</AccordionTrigger>
-                            <AccordionContent className="flex flex-col gap-4 text-balance">
+                            <AccordionContent className="flex flex-col gap-4">
 
-                                <div className="mb-5 flex justify-between flex-col gap-3 pl-3">
+                                <div className="mb-5 flex justify-between flex-col gap-3">
                                     <div className="flex gap-2">
-                                        <h1 className="font-semibold">Back Pressure - </h1>
+                                        <h1 className="font-semibold">Back Pressure</h1>
                                         <h1 className=" text-sm text-muted-foreground">Pulling your opponent&apos;s hand toward you.</h1>
                                     </div>
                                     <div className="flex gap-2">
-                                        <h1 className="font-semibold">Pronation - </h1>
+                                        <h1 className="font-semibold">Pronation</h1>
                                         <h1 className=" text-sm text-muted-foreground">Turning your hand so your knuckles roll over theirs.</h1>
                                     </div>
                                     <div className="flex gap-2">
-                                        <h1 className="font-semibold">Rising - </h1>
+                                        <h1 className="font-semibold">Rising</h1>
                                         <h1 className=" text-sm text-muted-foreground">Lifting your knuckles upward to weaken their grip.</h1>
                                     </div>
                                     <div className="flex gap-2">
-                                        <h1 className="font-semibold">Wrist Integrity - </h1>
+                                        <h1 className="font-semibold">Wrist Integrity</h1>
                                         <h1 className=" text-sm text-muted-foreground">Keeping your wrist straight or slightly cupped.</h1>
                                     </div>
                                 </div>
@@ -107,7 +107,7 @@ export default async function Page() {
 
                         {/* <AccordionItem value="item-3">
                             <AccordionTrigger>Trainings for Hook</AccordionTrigger>
-                            <AccordionContent className="flex flex-col gap-4 text-balance">
+                            <AccordionContent className="flex flex-col gap-4">
                                 {
                                     coc.map((data, index) =>
                                         <div key={index}>
--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI || "mongodb+srv://atlasUser:vXZn9UumkM6rTNCg@cluster0.yeukxf1.mongodb.net/titanarms?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI || "";
 
 const mongoClient = new MongoClient(uri);
 await mongoClient.connect();
