ca9e41938ae3da5a44fc8e6e5f836a03d4ecb306
kdlanguido <kdlanguido@gmail.com>
2026-01-23 04:42:57 +0800

    push to prod

--- app/(authenticated)/training/orientation/page.tsx
+++ app/(authenticated)/training/orientation/page.tsx
@@ -1,7 +1,5 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 
-export const dynamic = "force-static"
-
 export default function Page() {
 
     const items = [
